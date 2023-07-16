5575591901b7b1bd3434a16873bd91868f167bea
theimpostor <theimpostor@users.noreply.github.com>
2023-07-16 12:39:55 -0500

    add fuel gague

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const gravity = 0.01;
 const sideEngineThrust = 0.01;
 const mainEngineThrust = 0.03;
 const lzBuffer = 3;
+const initialFuel = 1000;
 
 class Rect {
   constructor(x, y, w, h) {
@@ -63,6 +64,9 @@ ship.leftEngine = false;
 ship.rightEngine = false;
 ship.crashed = false;
 ship.landed = false;
+ship.hasFuel = function () {
+  return this.fuel > 0;
+}
 
 const platform = new Rect(190, 345, 20, 5);
 platform.color = "blue";
@@ -109,6 +113,7 @@ function initShip() {
   ship.rightEngine = false;
   ship.crashed = false;
   ship.landed = false;
+  ship.fuel = initialFuel;
 }
 
 function drawPrjs() {
@@ -151,7 +156,7 @@ function drawShip() {
   ctx.closePath();
 
   // Draw the flame if engine is on
-  if (ship.mainEngine) {
+  if (ship.hasFuel() && ship.mainEngine) {
     drawTriangle(
       [ship.w * -0.5, ship.h * 0.5],
       [ship.w * 0.5, ship.h * 0.5],
@@ -159,7 +164,7 @@ function drawShip() {
       "orange"
     );
   }
-  if (ship.rightEngine) {
+  if (ship.hasFuel() && ship.rightEngine) {
     drawTriangle(
       [ship.w * 0.5, ship.h * -0.25],
       [ship.w * 0.5 + Math.random() * 10, 0],
@@ -167,7 +172,7 @@ function drawShip() {
       "orange"
     );
   }
-  if (ship.leftEngine) {
+  if (ship.hasFuel() && ship.leftEngine) {
     drawTriangle(
       [ship.w * -0.5, ship.h * -0.25],
       [ship.w * -0.5 - Math.random() * 10, 0],
@@ -183,19 +188,24 @@ function updateShip() {
   ship.dy += gravity;
 
   // what other forces acting on the ship?
-  if (ship.rightEngine) {
+  if (ship.hasFuel() && ship.rightEngine) {
     ship.dx -= sideEngineThrust;
+    ship.fuel -= sideEngineThrust * 100;
   }
-  if (ship.leftEngine) {
+  if (ship.hasFuel() && ship.leftEngine) {
     ship.dx += sideEngineThrust;
+    ship.fuel -= sideEngineThrust * 100;
   }
-  if (ship.mainEngine) {
+  if (ship.hasFuel() && ship.mainEngine) {
     ship.dy -= mainEngineThrust;
+    ship.fuel -= mainEngineThrust * 100;
   }
 
   // after calculating velocity, update our position
   ship.x += ship.dx;
   ship.y += ship.dy;
+
+  console.log("fuel", ship.fuel)
 }
 
 function updatePrjs() {
@@ -284,6 +294,7 @@ function gameLoop() {
   } else {
     // Clear entire screen
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillText(`fuel: ${Math.max(0, Math.floor(ship.fuel))}`, 2, 10)
     drawPrjs();
     drawShip();
     drawPlatform();
